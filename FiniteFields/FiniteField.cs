d22e48b7fd8a43fb83d328291620b3e0151d6f27
K0lba 0_o <a.rudskoj@icloud.com>
2023-03-06 19:07:24 +0300

    Добавьте файлы проекта.

---
+++ FiniteFields/FiniteField.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiniteFields
+{ 
+    public class FiniteField
+    {
+        public int n { get; set; }
+        public int p { get; set; }
+        public FiniteFieldElements q { get; set; }
+
+        public FiniteField(int n, int p, FiniteFieldElements q)
+        {
+            this.n = n;
+            this.p = p;
+            this.q = q;
+
+        }
+        public FiniteFieldElements Create0()
+        {
+            return new FiniteFieldElements(new int[1] { 0 }, this);
+        }
+        public FiniteFieldElements Create1()
+        {
+            return new FiniteFieldElements(new int[1] { 1 }, this);
+        }
+    }
+    public class FiniteFieldElements
+    {
+        public int[] coef { get; set; }
+        public FiniteField f { get; set; }
+
+        public FiniteFieldElements(int[] coef, FiniteField f)
+        {
+            this.coef = coef;
+            this.f = f;
+        }
+        
+        public static FiniteFieldElements operator -(FiniteFieldElements a)
+        {
+            int deg = a.f.n;
+            int[] result = new int[deg];
+            for(int i=0; i<=deg; i++)
+            {
+                result[i] = -a.coef[i];
+            }
+            return new FiniteFieldElements(result, a.f);
+        }
+
+        public static FiniteFieldElements operator +(FiniteFieldElements a, FiniteFieldElements b)
+        {
+            int deg = Math.Max(a.f.n, b.f.n);
+            int[] result = new int[deg];
+            for (int i = 0; i <= deg; i++)
+            {
+                result[i] = a.coef[i] + b.coef[i];
+            }
+            return new FiniteFieldElements(result, a.f);
+        }
+
+        public static FiniteFieldElements operator -(FiniteFieldElements a, FiniteFieldElements b) => a + (-b);
+
+        public static FiniteFieldElements operator %(FiniteFieldElements a, FiniteFieldElements b)
+        {
+
+            return new FiniteFieldElements(a.coef, a.f);
+        }
+        public static FiniteFieldElements operator *(FiniteFieldElements a, FiniteFieldElements b)
+        {
+            int deg = a.f.n + b.f.n;
+            int[] result = new int[deg];
+            for (int i = 0; i <= deg; i++)
+            {
+                for (int j = 0; j <= Math.Min(i, a.f.n); j++)
+                {
+                    result[i] += a.coef[j] * b.coef[i-j];
+                }
+            }
+            FiniteFieldElements res = new FiniteFieldElements(result,a.f) % a.f.q;    
+            return res;
+        }
+
+        public FiniteFieldElements Power(int power)
+        {
+
+            return this;
+        }
+    }
+}
